076adbfbd8dcccd73d6d74c44f037bbd7d4f7de4
user8595 <muhikhsansyakir@gmail.com>
2025-10-04 18:49:48 +0800

    add app icon

--- conf.lua
+++ conf.lua
@@ -1,8 +1,9 @@
 function love.conf(t)
     t.identity = "screen"
     t.window.title = "screen"
+    t.window.icon = "assets/icon.png"
     t.window.width = 640
-    t.window.height = 480
+    t.window.height = 480   
     t.window.borderless = true
     t.window.fullscreen = true
     t.version = "11.5"
